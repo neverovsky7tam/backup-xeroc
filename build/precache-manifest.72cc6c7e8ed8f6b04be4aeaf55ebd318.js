5f018150cbfe3f6e0ae856c41e901c1706e26527
Vladislav Priakhine <vladjislav@gmail.com>
2020-04-02 13:03:04 +0300

    build 2020/04/02

---
+++ build/precache-manifest.72cc6c7e8ed8f6b04be4aeaf55ebd318.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d0234563aea3df91f5f095b9a7d565e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24377cca1e38446cdee1",
+    "url": "/static/css/main.34a78916.chunk.css"
+  },
+  {
+    "revision": "823212bcf10474bc6b4d",
+    "url": "/static/js/2.1544a642.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1544a642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24377cca1e38446cdee1",
+    "url": "/static/js/main.c31622d3.chunk.js"
+  },
+  {
+    "revision": "220d64f6f8cb5da655ce",
+    "url": "/static/js/runtime-main.c730f065.js"
+  },
+  {
+    "revision": "9e1335cc83270fb06e5343f2b27f4e8e",
+    "url": "/static/media/alipay.9e1335cc.svg"
+  },
+  {
+    "revision": "7d060a9a6d70e90150b96823ec37efff",
+    "url": "/static/media/arrow-dropdown-menu.7d060a9a.svg"
+  },
+  {
+    "revision": "8f9b10a4ac778b29fe32a85910240c4b",
+    "url": "/static/media/balance-icon.8f9b10a4.svg"
+  },
+  {
+    "revision": "33aafa1c7649cf7717b77285ceba31ef",
+    "url": "/static/media/bell-icon.33aafa1c.svg"
+  },
+  {
+    "revision": "292f0070289fec1c15c7664d7225d4d2",
+    "url": "/static/media/cart-icon.292f0070.svg"
+  },
+  {
+    "revision": "7f8455b70734a87596991e1f24053edc",
+    "url": "/static/media/china.7f8455b7.svg"
+  },
+  {
+    "revision": "1363b83127e379b66530b206d02b01ac",
+    "url": "/static/media/close.1363b831.svg"
+  },
+  {
+    "revision": "d2beede8b4688e198759956bc3d28018",
+    "url": "/static/media/coinpayments.d2beede8.svg"
+  },
+  {
+    "revision": "2a877e1308d99a0f61b4355be225e2b5",
+    "url": "/static/media/corex-logo.2a877e13.svg"
+  },
+  {
+    "revision": "fbede6827b424f2e4aeaad21ecb74edb",
+    "url": "/static/media/facebook.fbede682.svg"
+  },
+  {
+    "revision": "07a425a1cba2f2e366fc0567486b156b",
+    "url": "/static/media/google.07a425a1.svg"
+  },
+  {
+    "revision": "0d21496a792d144125a3f2a3a38ff7af",
+    "url": "/static/media/info.0d21496a.svg"
+  },
+  {
+    "revision": "f5c2afd0e439960140336a5d9ffe9fd2",
+    "url": "/static/media/login-icon.f5c2afd0.svg"
+  },
+  {
+    "revision": "7e6b5075e545a03eba3b9755b83faa15",
+    "url": "/static/media/master-card.7e6b5075.svg"
+  },
+  {
+    "revision": "de3799ae817578fc09599a0b39b2948b",
+    "url": "/static/media/menu-separate.de3799ae.svg"
+  },
+  {
+    "revision": "544318ff764180221979bc6a7956352e",
+    "url": "/static/media/paypal.544318ff.svg"
+  },
+  {
+    "revision": "967c0272785620432526f6dc441c84cf",
+    "url": "/static/media/pencil-icon.967c0272.svg"
+  },
+  {
+    "revision": "337fcb353c25ee6497324408e0ac58aa",
+    "url": "/static/media/planet-earth.337fcb35.svg"
+  },
+  {
+    "revision": "10fbf738ad453567bc1bc58bff38f889",
+    "url": "/static/media/skrill.10fbf738.svg"
+  },
+  {
+    "revision": "690eae12791d6cab93e2d607b06035e3",
+    "url": "/static/media/telegram.690eae12.svg"
+  },
+  {
+    "revision": "dd26de2d012b5a9ff70a501df93cf345",
+    "url": "/static/media/twitter.dd26de2d.svg"
+  },
+  {
+    "revision": "36ab476e5e55f496749ee61897a9cfb5",
+    "url": "/static/media/united-states.36ab476e.svg"
+  },
+  {
+    "revision": "70da1de086f46549c43c0c58e2688613",
+    "url": "/static/media/user-icon.70da1de0.svg"
+  },
+  {
+    "revision": "a849f3888400c955e4f0c7f344d6528f",
+    "url": "/static/media/vector-star.a849f388.svg"
+  },
+  {
+    "revision": "e51cb6837ec4a4da85efb3ad8d3eab98",
+    "url": "/static/media/visa.e51cb683.svg"
+  },
+  {
+    "revision": "f94e8c6ae9f436a283c6cb3d235e7861",
+    "url": "/static/media/webmoney.f94e8c6a.svg"
+  }
+]);
