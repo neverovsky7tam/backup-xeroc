49383153db50f936b81e61081edeb70a56b11426
Vladislav Priakhine <vladjislav@gmail.com>
2020-06-05 11:54:01 +0300

    feat: 2560px 'listings' layout (XER-117: 180)

--- src/components/Main/Listings/Listings.js
+++ src/components/Main/Listings/Listings.js
@@ -5,36 +5,36 @@ import renderProducts from '../OnSale/renderProducts';
 import ProductTile from '../OnSale/ProductTile';
 
 const Listings = ({ productsObj }) => {
-  const data = productsObj.slice(0, 10);
+  const data = productsObj.slice(0, 12);
 
-  const scrollBlock = React.createRef();
+  // const scrollBlock = React.createRef();
 
-  const dispatch = useDispatch();
-  const scrollTop = useSelector((state) => state.scrollHeight);
+  // const dispatch = useDispatch();
+  // const scrollTop = useSelector((state) => state.scrollHeight);
 
   const listingsSectionCssClass = useSelector((state) => state.listingsSectionCssClass);
   const cssClass = (listingsSectionCssClass) ? listingsSectionCssClass : 'listings home-page';
 
-  useLayoutEffect(() => {
-    scrollBlock.current.scrollTop = scrollTop;
-  });
+  // useLayoutEffect(() => {
+  //   scrollBlock.current.scrollTop = scrollTop;
+  // });
 
-  const calcScrollState = () => {
-    const elem = scrollBlock.current;
-    const scrollBottom = elem.scrollHeight - elem.offsetHeight - elem.scrollTop;
+  // const calcScrollState = () => {
+  //   const elem = scrollBlock.current;
+  //   const scrollBottom = elem.scrollHeight - elem.offsetHeight - elem.scrollTop;
 
-    if (elem.scrollTop === 0) {
-      let mediumScrollHeight = elem.scrollHeight / 2;
-      if (mediumScrollHeight === scrollTop) mediumScrollHeight += 1;
-      dispatch(setCurrentScrollTop(mediumScrollHeight));
-    }
+  //   if (elem.scrollTop === 0) {
+  //     let mediumScrollHeight = elem.scrollHeight / 2;
+  //     if (mediumScrollHeight === scrollTop) mediumScrollHeight += 1;
+  //     dispatch(setCurrentScrollTop(mediumScrollHeight));
+  //   }
 
-    if (scrollBottom < 1) {
-      let currentScrollHeight = elem.scrollHeight / 2 - elem.offsetHeight;
-      if (currentScrollHeight === scrollTop) currentScrollHeight += 1;
-      dispatch(setCurrentScrollTop(currentScrollHeight));
-    }
-  }
+  //   if (scrollBottom < 1) {
+  //     let currentScrollHeight = elem.scrollHeight / 2 - elem.offsetHeight;
+  //     if (currentScrollHeight === scrollTop) currentScrollHeight += 1;
+  //     dispatch(setCurrentScrollTop(currentScrollHeight));
+  //   }
+  // }
 
   return (
     <section className={cssClass}>
@@ -43,9 +43,7 @@ const Listings = ({ productsObj }) => {
       </div>
       <div className="listings__body main-body">
         <ul
-          className="scroll-container"
-          ref={scrollBlock}
-          onScroll={calcScrollState}>
+          className="listings__list scroll-container">
           {renderProducts(ProductTile, data)}
         </ul>
       </div>
