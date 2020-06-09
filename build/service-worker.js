620ae962fa5a3a762dc3d15118e41d8a1c2eb3f4
Vladislav Priakhine <vladjislav@gmail.com>
2020-06-09 18:29:34 +0300

    build develop

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.78dad13255ce4117d2b381aea93d3836.js"
+  "/precache-manifest.2facd03eb2ff363b2ad74c5616333ed7.js"
 );
 
 self.addEventListener('message', (event) => {
