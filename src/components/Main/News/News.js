fa76f74959df932f8bc0cdaff6cedfef11b79ed5
Vladislav Priakhine <vladjislav@gmail.com>
2020-06-10 14:20:29 +0300

    fix: crossbrowsing fixes: banner responsive layout

--- src/components/Main/News/News.js
+++ src/components/Main/News/News.js
@@ -10,24 +10,26 @@ const News = () => {
         <h2>news</h2>
       </div>
       <div className="news__body main-body">
-        <div className="banner-wrapper p-relative">
-          <div className="banner">
-            <div className="banner__logo-buffer"></div>
-            <div className="banner__logo"><BannerLogo /></div>
-            <div className="banner__title-buffer"></div>
-            <h2>THE STANDARD FOR CRYPTO CONSULTING</h2>
-            <div className="banner__benefits-buffer"></div>
-            <ul>
-              <li>Mining Operations</li>
-              <li>ICO (Initial Coin Offering)</li>
-              <li>New Business Investment</li>
-              <li>Blockchain Technology</li>
-            </ul>
-            <div className="banner__illustration-buffer"></div>
-            <div className="banner__illustration"><Illustration /></div>
-            <div className="banner__bottom-buffer"></div>
+        <div className="scroll-container">
+          <div className="banner-wrapper">
+            <div className="banner">
+              <div className="banner__logo-buffer"></div>
+              <div className="banner__logo"><BannerLogo /></div>
+              <div className="banner__title-buffer"></div>
+              <h2>THE STANDARD FOR CRYPTO CONSULTING</h2>
+              <div className="banner__benefits-buffer"></div>
+              <ul>
+                <li>Mining Operations</li>
+                <li>ICO (Initial Coin Offering)</li>
+                <li>New Business Investment</li>
+                <li>Blockchain Technology</li>
+              </ul>
+              <div className="banner__illustration-buffer"></div>
+              <div className="banner__illustration"><Illustration /></div>
+              <div className="banner__bottom-buffer"></div>
+            </div>
+            <BoxDecor />
           </div>
-          <BoxDecor />
         </div>
       </div>
     </section>
