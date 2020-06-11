868d009b191094609fc1da894f2e9ba374d0faa9
Vladislav Priakhine <vladjislav@gmail.com>
2020-06-11 14:07:55 +0300

    feat: 'not supported browsers' notification XER-433

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React, {useLayoutEffect} from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setDeviceType } from './store/actions';
+import Plug from './components/Plug/Plug';
 import Header from './components/Header/Header';
 
 import MainContainer from './components/Main/MainContainer';
 import TermsCloseBtn from './components/Main/Terms/TermsCloseBtn';
 
 function App() {
+  console.log('check', window.navigator.userAgent);
+  const browser = window.navigator.userAgent;
+  const isYandexBro = browser.toLowerCase().includes('yabrowser');
+  const isIEBro = !!document.documentMode;
+
   const dispatch = useDispatch();
   const termsCloseBtn = useSelector((state) => state.termsCloseBtn)
 
@@ -15,13 +21,17 @@ function App() {
   if (isMobile) dispatch(setDeviceType(true));
   else dispatch(setDeviceType(false));
 
-  return (
-    <>
-      <Header isMobile={isMobile} />
-      <MainContainer />
-      {termsCloseBtn && <TermsCloseBtn />}
-    </>
-  );
+  if (isYandexBro || isIEBro) {
+    return <Plug />
+  } else {
+    return (
+      <>
+        <Header isMobile={isMobile} />
+        <MainContainer />
+        {termsCloseBtn && <TermsCloseBtn />}
+      </>
+    );
+  };
 };
 
 export default App;
