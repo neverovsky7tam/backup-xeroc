fa680d5ef19f47ca9f48574e576adfcec17218a3
Vladislav Priakhine <vladjislav@gmail.com>
2020-08-14 16:47:44 +0300

    feat: [mobile] empty cart

--- src/modules/Cart/components/CartEmpty.js
+++ src/modules/Cart/components/CartEmpty.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Container from 'components/BlocksUI/Container';
+import { NotFound } from 'svg/svg';
 
 const CartEmpty = () => {
 	return (
-		<div className="main-font">Epmty</div>
-  );
+		<div className="cart-empty">
+			<Container>
+				<div className="content">
+					{NotFound}
+					<span className="main-font">You have 0 items in your cart</span>
+				</div>
+			</Container>
+		</div>
+	);
 };
 
 export default CartEmpty;
--- src/modules/Cart/components/CartFooter.js
+++ src/modules/Cart/components/CartFooter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { clearCart } from 'store/actions';
+import { clearCart, setCartState } from 'store/actions';
 import { ButtonGrey, ButtonMain } from 'components/BlocksUI/Buttons/Buttons';
 import Container from 'components/BlocksUI/Container';
 import { DeleteIcon } from 'svg/svg';
@@ -8,20 +8,31 @@ import { DeleteIcon } from 'svg/svg';
 const CartFooter = ({ totalPrice }) => {
   const dispatch = useDispatch();
 
+  const CartControls = (
+    <div className="btns-holder">
+      <ButtonGrey
+        customClass='btn-del'
+        value={DeleteIcon}
+        func={() => dispatch(clearCart())} />
+      <Container>
+        <div className="price-display">
+          ${totalPrice}
+        </div>
+      </Container>
+      <ButtonMain text='Buy' />
+    </div>
+  );
+
+  const EmptyCartBtn = (
+    <ButtonMain
+      text={'Back to shopping'}
+      func={() => dispatch(setCartState(false))} />
+  );
+
   return (
     <div className="cart__footer">
-      <div className="btns-holder">
-        <ButtonGrey
-          customClass='btn-del'
-          value={DeleteIcon} 
-          func={() => dispatch(clearCart())}/>
-        <Container>
-          <div className="price-display">
-            ${totalPrice}
-          </div>
-        </Container>
-        <ButtonMain text='Buy' />
-      </div>
+      {(totalPrice) ?
+        CartControls : EmptyCartBtn}
     </div>
   );
 };
